33f5608d2256a4ef671a2e022cd78c3e3d44d1a1
Digui87 <d_salvador110@outlook.com>
2023-10-27 10:59:54 -0300

    commit inicial/final

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import store from './store'
+
+import EstiloGlobal from './styles'
+import { Container } from './styles'
+import Home from './pages/home'
+import AdicionarContatos from './pages/adicionarContatos'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/adicionar-contato',
+    element: <AdicionarContatos />
+  }
+])
+
 function App() {
-  return <></>
+  return (
+    <Provider store={store}>
+      <EstiloGlobal />
+      <Container>
+        <RouterProvider router={rotas} />
+      </Container>
+    </Provider>
+  )
 }
 
 export default App
--- src/styles/variaveis.ts
+++ src/styles/variaveis.ts
@@ -1,6 +1,9 @@
 export default {
-  corFundo: '#202124',
-  corTexto: '#fff',
-  corPrincipal: '#1B1464',
-  corSecundaria: '#FFC312'
+  azulEscuro: '#0652DD',
+  azulClaro: '#0097e6',
+  vermelhoEscuro: 'rgb(128, 10, 10)',
+  vermelhoClaro: '#EA2027',
+  cinzaEscuro: '#2f3542',
+  cinza: '#3d3d3d',
+  shadow: 'rgba(0, 0, 0, 0.4)'
 }
